b5263b3889446e51fc7f9e5d8ae40fc6d6d4645e
abdulatifkhan <anvarkhanov96@gmail.com>
2020-11-26 18:45:11 +0500

    Add script fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-// Correct the names in the Movies array
-
-/* var compactMovies = movies.map(function (movie, i) {
-  return {
-    id: i + 1,
-    title: movie.Title.toString(),
-    year: movie.movie_year,
-    categories: movie.Categories.split('|'),
-    summary: movie.summary,
-    imdbId: movie.imdb_id,
-    imdbRating: movie.imdb_rating,
-    runtime: movie.rumtime,
-    language: movie.language,
-    // trailer: `https://www.youtube.com/watch?v=${movie.ytid}`,
-    trailer: getYouTubeVideoLink(movie.ytid),
-    // imageUrl: `http://i3.ytimg.com/vi/${movie.ytid}/hqdefault.jpg`,
-    imageUrl: getYouTubeVideoThumbnail(movie.ytid),
-    // bigImageUrl: `http://i3.ytimg.com/vi/${movie.ytid}/maxresdefault.jpg`
-    bigImageUrl: getYouTubeVideoBigThumbnail(movie.ytid),
-  };
-}); */
-
 // Announcing the Form
 
 var elMoviesForm = $_('.js-movies-form');
@@ -33,53 +11,165 @@ var elMoviesSorts = $_('.js-movies-sorts', elMoviesForm);
 var elMoviesList = $_('.js-movies-list');
 var elMoviesTemplate = $_('#movie-template').content;
 
-var createMoviesElement = function (compactMovies) {
-  var elNewMovie = elMoviesTemplate.cloneNode(true);
+var createGenereSelectOptions = function () {
+  var movieCategories = [];
 
-  $_('.movie-title', elNewMovie).textContent = compactMovies.title;
-  $_('.movie-image', elNewMovie).src = compactMovies.imageUrl;
-  $_('.movie-image', elNewMovie).alt = compactMovies.title;
-  $_('.movie-year', elNewMovie).textContent = compactMovies.year;
-  $_('.movie-categories', elNewMovie).textContent = compactMovies.categories;
-  $_('.movie-summary', elNewMovie).textContent = compactMovies.summary;
-  $_('.movie-imdb-rating', elNewMovie).textContent = compactMovies.imdbRating;
-  $_('.movie-language', elNewMovie).textContent = compactMovies.language;
-  $_('.movie-youtube', elNewMovie).href = compactMovies.trailer;
+  compactMovies.slice(0, 50).forEach(function (movie) {
+    movie.categories.forEach(function (category) {
+      if (!movieCategories.includes(category)) {
+        movieCategories.push(category);
+      }
+    });
+  });
 
-  return elNewMovie;
+  movieCategories.sort();
+
+  var elOptionsFragment = document.createDocumentFragment();
+
+  movieCategories.forEach(function (category) {
+    // var elCategoryOption = document.createElement('option');
+    // elCategoryOption.textContent = category;
+    var elCategoryOption = createElement('option', '', category);
+
+    elCategoryOption.value = category.toLowerCase();
+
+    elOptionsFragment.appendChild(elCategoryOption);
+  });
+  
+  elMoviesCategories.appendChild(elOptionsFragment);
 };
 
-// Announcing the Fragment
+createGenereSelectOptions();
 
-var renderMovies = function (compactMovies) {
+/* 
+ES5
+
+elMoviesForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+}); 
+*/
+
+var renderResults = function (searchResults) {
   elMoviesList.innerHTML = '';
 
-  var elMoviesWrapperFragment = document.createDocumentFragment();
+  var elResultsFragment = document.createDocumentFragment();
 
-  compactMovies.forEach(function (movie) {
-    elMoviesWrapperFragment.appendChild(createMoviesElement(movie));
+  searchResults.forEach((movie) => {
+    var elMovie = elMoviesTemplate.cloneNode(true);
+    $_('.movie-title', elMovie).textContent = movie.title;
+    $_('.movie-image', elMovie).src = movie.imageUrl;
+    $_('.movie-image', elMovie).alt = movie.title;
+    $_('.movie-year', elMovie).textContent = movie.year;
+    $_('.movie-categories', elMovie).textContent = movie.categories;
+    $_('.movie-summary', elMovie).textContent = movie.summary;
+    $_('.movie-imdb-rating', elMovie).textContent = movie.imdbRating;
+    $_('.movie-language', elMovie).textContent = movie.language;
+    $_('.movie-youtube', elMovie).href = movie.trailer;
+
+
+    elResultsFragment.appendChild(elMovie);
   });
 
-  elMoviesList.appendChild(elMoviesWrapperFragment);
+  elMoviesList.appendChild(elResultsFragment);
 };
 
-renderMovies(compactMovies.slice(0, 200));
-
-// for (var category of categories)
-
-// Announcing the Form function
-
-var searchMovies = function (evt) {
+// ES6
+elMoviesForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
-  var searchRegaxValue = elMoviesInput.value.trim();
-  var searchRegax = new RegExp(searchRegaxValue, 'gi');
-  
-  var findMoviesBySearch = compactMovies.filter (function (movie) {
-    return movie.title.match(searchRegax);
+  var searchTitle = elMoviesInput.value.trim();
+  var movieTitleRegex =  new RegExp(searchTitle, 'gi');
+
+  var searchResults = compactMovies.filter((movie) => {
+    return movie.title.match(movieTitleRegex);
   });
 
-  renderMovies(findMoviesBySearch);
-};
+  renderResults(searchResults);
+});
 
-elMoviesForm.addEventListener('submit', searchMovies);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var createMoviesElement = function (compactMovies) {
+//   var elNewMovie = elMoviesTemplate.cloneNode(true);
+
+//   $_('.movie-title', elNewMovie).textContent = compactMovies.title;
+//   $_('.movie-image', elNewMovie).src = compactMovies.imageUrl;
+//   $_('.movie-image', elNewMovie).alt = compactMovies.title;
+//   $_('.movie-year', elNewMovie).textContent = compactMovies.year;
+//   $_('.movie-categories', elNewMovie).textContent = compactMovies.categories;
+//   $_('.movie-summary', elNewMovie).textContent = compactMovies.summary;
+//   $_('.movie-imdb-rating', elNewMovie).textContent = compactMovies.imdbRating;
+//   $_('.movie-language', elNewMovie).textContent = compactMovies.language;
+//   $_('.movie-youtube', elNewMovie).href = compactMovies.trailer;
+
+//   return elNewMovie;
+// };
+
+// // Announcing the Fragment
+
+// var renderMovies = function (compactMovies) {
+//   elMoviesList.innerHTML = '';
+
+//   var elMoviesWrapperFragment = document.createDocumentFragment();
+
+//   compactMovies.forEach(function (movie) {
+//     elMoviesWrapperFragment.appendChild(createMoviesElement(movie));
+//   });
+
+//   elMoviesList.appendChild(elMoviesWrapperFragment);
+// };
+
+// renderMovies(compactMovies.slice(0, 200));
+
+// // for (var category of categories)
+
+// // Announcing the Form function
+
+// var searchMovies = function (evt) {
+//   evt.preventDefault();
+
+//   var searchRegaxValue = elMoviesInput.value.trim();
+//   var searchRegax = new RegExp(searchRegaxValue, 'gi');
+  
+//   var findMoviesBySearch = compactMovies.filter (function (movie) {
+//     return movie.title.match(searchRegax);
+//   });
+
+//   renderMovies(findMoviesBySearch);
+// };
+
+// elMoviesForm.addEventListener('submit', searchMovies);
